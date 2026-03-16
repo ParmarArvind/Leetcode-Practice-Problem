276534f7c6bb49997859c02fa3b0f94907d1cd89
Arvind Parmar <arvind1057956@gmail.com>
2026-03-16 12:00:14 +0530

    Create LC-215. Kth Largest Element in an Array..cpp

---
+++ LC-215. Kth Largest Element in an Array..cpp
@@ -0,0 +1,29 @@
+// with sorting 
+class Solution {
+public:
+    int findKthLargest(vector<int>& nums, int k) {
+        sort(nums.begin(),nums.end(),greater<int>());
+        
+        return nums[k-1];
+    }
+};
+
+//without sorting 
+class Solution {
+public:
+    int findKthLargest(vector<int>& nums, int k) {
+        priority_queue<int >pq;
+        int n=nums.size();
+        for(int i=0;i<n;i++)
+        {
+            pq.push(nums[i]);
+        }
+        for(int i=1;i<k;i++)
+        {
+            pq.pop();
+        }
+        return pq.top();
+
+        
+    }
+};
