da213b0b2d29856f594d49cb2a0cc3484f27c3a4
Arvind Parmar <arvind1057956@gmail.com>
2026-03-29 02:22:58 +0530

    Update 01 Zero Array Transformation I(LC-3355).cpp

--- Array/01 Zero Array Transformation I(LC-3355).cpp
+++ Array/01 Zero Array Transformation I(LC-3355).cpp
@@ -1,25 +1,38 @@
+//Approach - Straight Forward Difference Array Technique
+//T.C : O(Q + n)
+//S.C : O(Q + n)
 class Solution {
 public:
     bool isZeroArray(vector<int>& nums, vector<vector<int>>& queries) {
-        int n=nums.size();
-        int m=queries.size();
-        vector<int> shift(n,0);
+        int n = nums.size();
 
-        for(int i=0;i<m;i++)
-        {
-            int start=queries[i][0];
-            int end=queries[i][1];
-            shift[start]++;
-            if(end+1<n) shift[end+1] -=1;
-        }
-        for(int i=1;i<n;i++){
-            shift[i]=shift[i]+shift[i-1];
+        //Step-1 Make diff array using query
+        vector<int> diff(n, 0);
+        for(auto &query : queries) {
+            int start = query[0];
+            int end   = query[1];
+            int x     = 1;
+
+            diff[start] += x;
+            if(end+1 < n) {
+                diff[end+1] -= x;
+            }
         }
 
-        for(int i=0;i<n;i++){
-            if(nums[i]>shift[i]) return false;
+        //Step-2. Find cumulative effect on each index
+        vector<int> result(n, 0);
+        int cumSum = 0;
+        for(int i = 0; i < n; i++) {
+            cumSum += diff[i];
+            result[i] = cumSum;
         }
+
+        for(int i = 0; i < n; i++) {
+            if(result[i] < nums[i]) { //nums[i] won't become 0 ever
+                return false;
+            }
+        }
+
         return true;
-
     }
 };
